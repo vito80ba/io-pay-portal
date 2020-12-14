5a5bc1c0208c02cceb57c193e2aee6d3d8a4a9c6
pasqualespica <pasqualespica@gmail.com>
2020-12-14 16:01:36 +0100

    [#175899260] init template fn

---
+++ io-functions-pay-portal/utils/healthcheck.ts
@@ -0,0 +1,64 @@
+import {
+  common as azurestorageCommon,
+  createBlobService,
+  createFileService,
+  createQueueService,
+  createTableService
+} from "azure-storage";
+import { sequenceT } from "fp-ts/lib/Apply";
+import { array } from "fp-ts/lib/Array";
+import { toError } from "fp-ts/lib/Either";
+import {
+  fromEither,
+  taskEither,
+  TaskEither,
+  tryCatch
+} from "fp-ts/lib/TaskEither";
+import { readableReport } from "italia-ts-commons/lib/reporters";
+import fetch from "node-fetch";
+import { getConfig, IConfig } from "./config";
+
+type ProblemSource = "AzureCosmosDB" | "AzureStorage" | "Config" | "Url";
+export type HealthProblem<S extends ProblemSource> = string & { __source: S };
+export type HealthCheck<
+  S extends ProblemSource = ProblemSource,
+  T = true
+> = TaskEither<ReadonlyArray<HealthProblem<S>>, T>;
+
+// format and cast a problem message with its source
+const formatProblem = <S extends ProblemSource>(
+  source: S,
+  message: string
+): HealthProblem<S> => `${source}|${message}` as HealthProblem<S>;
+
+// utility to format an unknown error to an arry of HealthProblem
+const toHealthProblems = <S extends ProblemSource>(source: S) => (
+  e: unknown
+): ReadonlyArray<HealthProblem<S>> => [
+  formatProblem(source, toError(e).message)
+];
+
+/**
+ * Check application's configuration is correct
+ *
+ * @returns either true or an array of error messages
+ */
+export const checkConfigHealth = (): HealthCheck<"Config", IConfig> =>
+  fromEither(getConfig()).mapLeft(errors =>
+    errors.map(e =>
+      // give each problem its own line
+      formatProblem("Config", readableReport([e]))
+    )
+  );
+
+/**
+ * Check a url is reachable
+ *
+ * @param url url to connect with
+ *
+ * @returns either true or an array of error messages
+ */
+export const checkUrlHealth = (url: string): HealthCheck<"Url", true> =>
+  tryCatch(() => fetch(url, { method: "HEAD" }), toHealthProblems("Url")).map(
+    _ => true
+  );
