a6a1784ceb9c27ccccf0747804e339f3e3300855
Simone Infante <simone.infante@pagopa.it>
2020-12-15 09:55:16 +0100

    Remove mocks folder

--- io-functions-pay-portal/__mocks__/durable-functions.ts
+++
@@ -1,9 +0,0 @@
-export const mockStartNew = jest.fn();
-
-export const getClient = jest.fn(() => ({
-  startNew: mockStartNew
-}));
-
-export const orchestrator = jest.fn();
-
-export const RetryOptions = jest.fn(() => ({}));
