f7ea0ca90dfc0829ee7ccb72ab70e98f3b2ec405
Simone Infante <simone.infante@pagopa.it>
2021-01-07 09:46:29 +0100

    [#175844896] Add description for dev server

--- io-pay-portal-fe/dev-server.js
+++ io-pay-portal-fe/dev-server.js
@@ -1,18 +1,30 @@
-const Bundler = require('parcel-bundler');
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+/**
+ * Development server built as an express application,
+ * able to run frontend (thanks to parcel-bundler) and an API server proxy (thanks to http-proxy-middleware)
+ * on localhost:1234, in order to you don't have to deal with CORS.
+ *
+ * Note: to run the development server must be set IO_PAY_PORTAL_FUNCTION=http://localhost:1234
+ * and apiHost with the host api (for example http://localhost:80).
+ */
 
+const Bundler = require("parcel-bundler");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use(createProxyMiddleware('/api/payportal/v1', {
-  target: 'http://localhost:80',
-  pathRewrite: {
-    '^/api/payportal/v1': '/api/v1',
-  },
-}));
+const apiHost = "http://localhost:80";
 
-const bundler = new Bundler('src/index.html');
+app.use(
+  createProxyMiddleware("/api/payportal/v1", {
+    target: apiHost,
+    pathRewrite: {
+      "^/api/payportal/v1": "/api/v1",
+    },
+  })
+);
+
+const bundler = new Bundler("src/index.html");
 app.use(bundler.middleware());
 
 app.listen(Number(1234));
