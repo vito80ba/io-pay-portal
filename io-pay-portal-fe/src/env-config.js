e9e9750a0ed7973397f923e68a09337b63681daa
Vito Falco <vitofalco@gmail.com>
2021-02-19 17:16:51 +0100

    Added support for scss. Added support for PUG. Refactor of index in PUG. Added assets & icons. Dropped jquery & refactor with vanilla JS. Added style like IO-PAY

--- io-pay-portal-fe/src/env-config.js
+++ io-pay-portal-fe/src/env-config.js
@@ -1,3 +1,7 @@
 window._env_ = {
-  // To be completed by env.sh
-};
+  IO_PAY_PORTAL_API_HOST: "http://localhost:1234",
+  IO_PAY_PORTAL_PAY_WL_HOST: "http://localhost:1234",
+  IO_PAY_PORTAL_API_REQUEST_TIMEOUT: "5000",
+  IO_PAY_PORTAL_PAY_WL_POLLING_ATTEMPTS: "10",
+  IO_PAY_PORTAL_PAY_WL_POLLING_INTERVAL: "3000",
+}
