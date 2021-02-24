fc0ccce53e1114916a578347a4e183c85d1982f3
Simone Infante <simone.infante@pagopa.it>
2021-02-24 17:48:56 +0100

    [#176942427] Add post methodResume and challengeResume for 3ds2

---
+++ io-functions-pay-portal/PostTransactionsMethod3ds2/index.ts
@@ -0,0 +1,40 @@
+import * as express from "express";
+import * as winston from "winston";
+
+import { Context } from "@azure/functions";
+
+import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
+import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
+import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
+import { methodPage } from "../utils/page3ds";
+
+// tslint:disable-next-line: no-let
+let logger: Context["log"] | undefined;
+const contextTransport = new AzureContextTransport(() => logger, {
+  level: "debug"
+});
+winston.add(contextTransport);
+
+// Setup Express
+const app = express();
+
+secureExpressApp(app);
+
+// Add express route
+
+app.post("/api/v1/transactions/:id/method/", (_, res) => {
+  res.set("Content-Type", "text/html");
+  return res.send(methodPage);
+});
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// Binds the express app to an Azure Function handler
+function httpStart(context: Context): void {
+  logger = context.log;
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
+
+export default httpStart;
