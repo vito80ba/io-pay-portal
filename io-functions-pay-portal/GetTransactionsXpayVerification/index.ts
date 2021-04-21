184bda3abe975157a5cccf0fbc54a24844c54b17
Simone Infante <simone.infante@pagopa.it>
2021-04-21 15:36:27 +0200

    fix lint

--- io-functions-pay-portal/GetTransactionsXpayVerification/index.ts
+++ io-functions-pay-portal/GetTransactionsXpayVerification/index.ts
@@ -3,7 +3,6 @@ import * as winston from "winston";
 
 import { Context } from "@azure/functions";
 
-import { fromNullable } from "fp-ts/lib/Option";
 import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
 import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
 import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
