8387df2a902bda43c92f7999c1d988c0c38abf48
Simone Infante <simone.infante@pagopa.it>
2021-05-04 10:44:21 +0200

    add new tests

--- io-functions-pay-portal/utils/tests/pagopaProxyUtil.test.ts
+++ io-functions-pay-portal/utils/tests/pagopaProxyUtil.test.ts
@@ -7,7 +7,7 @@ afterEach(() => {
 });
 
 describe("pagopaProxyUtil", () => {
-  it("should return IResponseErrorValidation if detail exists in error response", async () => {
+  it("should return IResponseErrorValidation if detail exists in error response", () => {
     const error: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 500,
@@ -17,7 +17,7 @@ describe("pagopaProxyUtil", () => {
     expect(error.kind).toBe("IResponseErrorValidation");
   });
 
-  it("should return IResponseErrorValidation if detail exists in error response", async () => {
+  it("should return IResponseErrorInternal if detail does not exists in error response", () => {
     const error: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 500,
@@ -27,43 +27,43 @@ describe("pagopaProxyUtil", () => {
     expect(error.kind).toBe("IResponseErrorInternal");
   });
 
-  it("should return IResponseErrorUnauthorized if response status is 401", async () => {
-    const error: ErrorResponses = toErrorPagopaProxyResponse({
+  it("should return IResponseErrorUnauthorized if response status is 401 ", () => {
+    const error401: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 401,
       value: {}
     });
 
-    expect(error.kind).toBe("IResponseErrorUnauthorized");
+    expect(error401.kind).toBe("IResponseErrorUnauthorized");
   });
 
-  it("should return IResponseErrorUnauthorized if response status is 403", async () => {
-    const error: ErrorResponses = toErrorPagopaProxyResponse({
+  it("should return IResponseErrorUnauthorized if response status is 403", () => {
+    const error403: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 403,
       value: {}
     });
 
-    expect(error.kind).toBe("IResponseErrorForbiddenNotAuthorized");
+    expect(error403.kind).toBe("IResponseErrorForbiddenNotAuthorized");
   });
 
-  it("should return ResponseErrorNotFound if response status is 404", async () => {
-    const error: ErrorResponses = toErrorPagopaProxyResponse({
+  it("should return ResponseErrorNotFound if response status is 404", () => {
+    const error404: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 404,
       value: {}
     });
 
-    expect(error.kind).toBe("IResponseErrorNotFound");
+    expect(error404.kind).toBe("IResponseErrorNotFound");
   });
 
-  it("should return ResponseErrorNotFound if response status is 429", async () => {
-    const error: ErrorResponses = toErrorPagopaProxyResponse({
+  it("should return ResponseErrorNotFound if response status is 429", () => {
+    const error429: ErrorResponses = toErrorPagopaProxyResponse({
       headers: {},
       status: 429,
       value: {}
     });
 
-    expect(error.kind).toBe("IResponseErrorTooManyRequests");
+    expect(error429.kind).toBe("IResponseErrorTooManyRequests");
   });
 });
