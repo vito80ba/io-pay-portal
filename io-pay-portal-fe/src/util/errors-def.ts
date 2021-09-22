72b97ddeebf2db670237804738c2dce046148519
Jacopo Pompilii <Jacopo.pompilii@pagopa.it>
2021-09-22 09:35:35 +0200

    Update errors-def.ts
    CTA fix

--- io-pay-portal-fe/src/util/errors-def.ts
+++ io-pay-portal-fe/src/util/errors-def.ts
@@ -242,22 +242,6 @@ export const PaymentResponses: Record<string, PaymentFaultErrorMessage> = {
     body:
       "La ricevuta è stata inviata all'indirizzo email che hai indicato durante il pagamento.",
     category: PaymentFaultCategory.CUSTOM,
-    buttons: [
-      {
-        title: "Contatta l'assistenza",
-        style: "btn btn-primary w-100 mb-2",
-        action: () => {
-          window.open(HELPDESK_URL, "_blank")?.focus();
-        },
-      },
-      {
-        title: "Chiudi",
-        style: "btn btn-outline-primary w-100",
-        action: (modalWindow: Tingle.modal) => {
-          modalWindow.close();
-        },
-      },
-    ],
   },
   PAA_PAGAMENTO_IN_CORSO: {
     title: "Il pagamento è già in corso, riprova tra qualche minuto",
