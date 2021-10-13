308e603331a70c15b110ecc1e7d6ca4648bdb868
Simone Infante <simone.infante@pagopa.it>
2021-10-13 12:03:46 +0200

    add coverage result

--- io-pay-portal-fe/jest.config.js
+++ io-pay-portal-fe/jest.config.js
@@ -2,11 +2,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["dist", "/node_modules"],
-  reporters: [
-    'default',
-    [ 'jest-junit', {
-      outputDirectory: './test_reports',
-      outputName: 'io-pay-portal-fe-ui-TEST.xml',
-    } ]
-  ]
 };
