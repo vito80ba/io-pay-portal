183e463d3e3e98907bd8e016aebeeab72da8267f
Nazar Chekalin <nazcheck@gmail.com>
2022-01-11 18:28:23 +0100

    Edit header

--- io-pay-portal-fe/src/components/commons/Header.tsx
+++ io-pay-portal-fe/src/components/commons/Header.tsx
@@ -1,17 +1,24 @@
-import React, { FunctionComponent } from "react";
 import { Box, BoxProps } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import LanguageToggleButton from "../LanguageToggleButton/LanguageToggleButton";
+import React, { FunctionComponent } from "react";
+import pagopaLogo from "../../assets/images/pagopa-logo.svg";
+import LanguageMenu from "../LanguageMenu/LanguageMenu";
 
 export const Header: FunctionComponent<BoxProps> = () => (
   <Box
     p={3}
     bgcolor={"white"}
-    sx={{ position: { xs: "fixed", sm: "relative" } }}
+    sx={{
+      position: { xs: "fixed", sm: "relative" },
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    }}
   >
-    <Typography variant="h3" gutterBottom component="div">
-      pagoPA
-    </Typography>
-    <LanguageToggleButton />
+    <img
+      src={pagopaLogo}
+      alt="pagoPA"
+      style={{ width: "56px", height: "36px" }}
+    />
+    <LanguageMenu />
   </Box>
 );
