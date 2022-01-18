3602027b67388e76692d73f06b4b7a507db479ab
Nazar Chekalin <nazcheck@gmail.com>
2022-01-18 10:35:28 +0100

    Setup redux store

---
+++ io-pay-portal-fe/src/features/payment/slices/paymentSlice.ts
@@ -0,0 +1,29 @@
+/* eslint-disable functional/immutable-data */
+import { createSlice } from "@reduxjs/toolkit";
+
+export const paymentSlice = createSlice({
+  name: "payment",
+  initialState: {
+    amount: 0,
+    creditor: "",
+    causal: "",
+    cf: 0,
+  },
+  reducers: {
+    setPayment(state, action) {
+      state.amount = action.payload.amount;
+      state.creditor = action.payload.creditor;
+      state.causal = action.payload.causal;
+      state.cf = action.payload.cf;
+    },
+    resetPayment(state) {
+      state.amount = 0;
+      state.creditor = "";
+      state.causal = "";
+      state.cf = 0;
+    },
+  },
+});
+
+export const { setPayment, resetPayment } = paymentSlice.actions;
+export default paymentSlice.reducer;
