51113ff9e8466506fd7c068f12cf6f811be2edfc
Nazar Chekalin <nazcheck@gmail.com>
2022-02-09 17:24:41 +0100

    Add binding payment notice - api

--- io-pay-portal-fe/src/utils/form/formatters.tsx
+++ io-pay-portal-fe/src/utils/form/formatters.tsx
@@ -8,3 +8,10 @@ export function expireDateFormatter(old: string, current: string) {
 export function cleanSpaces(text: string) {
   return text.replace(/\s/g, "");
 }
+
+export function moneyFormat(amount: number) {
+  return new Intl.NumberFormat("it-IT", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount / 100);
+}
